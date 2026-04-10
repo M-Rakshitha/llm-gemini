6a3e814f44db6b08204142ca25f2fe466060925e
R-Mamilla <mamillarakshitha@gmail.com>
2026-04-09 23:39:34 -0400

    Basic RAG

--- llm.py
+++ llm.py
@@ -1,46 +1,38 @@
 import json
 from google import genai
 from dotenv import load_dotenv
+from docx import Document
 load_dotenv()
 
 client = genai.Client()
-resume = """
-Software Engineer with experience in Python, AWS, and backend development.
-Built APIs and worked on data pipelines.
-"""
+
+def load_docx(file_path):
+    doc = Document(file_path)
+    text = ""
+
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+
+    return text
+        
+data = load_docx("data.docx")
+
+question = input("Ask a question: ")
+
 prompt = f"""
-You are a strict resume reviewer.
+Answer the question based ONLY on the context below.
 
-Rules:
-- Be critical
-- No generic answers
-- Give specific suggestions
+Context:
+{data}
 
-Return output in JSON:
-{{
-  "strengths": [],
-  "weaknesses": [],
-  "suggestions": []
-}}
+Question:
+{question}
 
-Resume:
-{resume}
+If the answer is not in the context, say "I don't know".
 """
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", 
     contents=prompt
 )
-clean_text = response.text.strip()
-
-if "```" in clean_text:
-    clean_text = clean_text.split("```")[1]
-    clean_text = clean_text.replace("json", "").strip()
-
-try:
-    data = json.loads(clean_text)
-    for s in data["strengths"]:
-      print("-", s)
-except json.JSONDecodeError:
-    print("Failed to parse JSON:")
-    print(clean_text)
+print(response.text)
